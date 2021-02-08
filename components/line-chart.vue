17b9008b1c20d0192016917b4f6ec63ff6c3c98e
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-02-08 23:04:58 +0700

    chart

--- components/line-chart.vue
+++ components/line-chart.vue
@@ -1,39 +1,46 @@
 <script>
-import { Line } from "vue-chartjs";
+import { Line, mixins } from "vue-chartjs";
+const { reactiveProp } = mixins
 
 export default {
   extends: Line,
+  mixins: [reactiveProp],
+  props: ['options'],
   mounted() {
-    this.renderChart(
-      {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
-        datasets: [
-          {
-            label: "Data 1",
-            data: [2, 10, 5, 9, 0, 6, 20],
-            backgroundColor: "transparent",
-            borderColor: "rgba(1, 116, 188, 0.50)",
-            pointBackgroundColor: "rgba(171, 71, 188, 1)"
-          }
-        ]
-      },
-      {
-        responsive: true,
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: "My Data"
-        }
-      }
-    );
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options);
+
+    // this.renderChart(
+    //   {
+    //     labels: [
+    //       "1 Januari",
+    //       "5 Januari",
+    //       "10 Januari",
+    //       "15 Januari",
+    //       "20 Januari",
+    //       "25 Januari",
+    //       "30 Januari"
+    //     ],
+    //     datasets: [
+    //       {
+    //         label: "Total Kasus",
+    //         data: [2, 10, 5, 9, 0, 6, 20],
+    //         backgroundColor: "transparent",
+    //         borderColor: "rgba(1, 116, 188, 0.50)",
+    //         pointBackgroundColor: "rgba(171, 71, 188, 1)"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     responsive: true,
+    //     maintainAspectRatio: false,
+    //     title: {
+    //       display: false,
+    //       text: "My Data"
+    //     }
+    //   }
+    // );
   }
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,103 @@
 <template>
-  <div class="max-w-xl mx-auto">
+  <div class="max-w-3xl pb-16 mx-auto">
     <div class="p-4 text-center">
-      <h1 class="text-3xl text-indigo-600 font-bold">
+      <h1 class="text-3xl font-bold text-indigo-600">
         Peramalan Kasus Positif Covid 19 di Indonesia
       </h1>
     </div>
-    <div>
-      <line-chart ></line-chart>
+    <div v-if="covid" class="mt-6 text-center">
+      <h2 class="text-2xl font-bold text-indigo-500">Bulan Januari 2021</h2>
+      <span class="text-sm italic text-indigo-400">Metode Fuzzy Time Series Chen</span>
+      <line-chart class="mt-4" :chart-data="chartdata"></line-chart>
     </div>
   </div>
 </template>
 
 <script>
 import LineChart from "@/components/line-chart";
+import { range } from "lodash";
 
 export default {
   components: {
-    'line-chart': LineChart
+    "line-chart": LineChart
+  },
+  data() {
+    return {
+      covid: null,
+      chartdata: null
+    };
+  },
+  created() {
+    this.predictCovid();
+  },
+  methods: {
+    async predictCovid() {
+      let train = [
+        751270,
+        758473,
+        765350,
+        772103,
+        779548,
+        788402,
+        797723,
+        808340,
+        818386,
+        828026,
+        836718,
+        846765,
+        858043,
+        869600,
+        882418,
+        896642,
+        907929,
+        917015,
+        927380,
+        939948,
+        951651,
+        965283,
+        977474,
+        989262,
+        999256,
+        1012350,
+        1024298,
+        1037993,
+        1051795,
+        1066313
+      ];
+      try {
+        this.covid = await this.$axios.post(
+          "https://django-fts.herokuapp.com/fts/predict",
+          {
+            train,
+            test: [...train],
+            mode: "chen"
+          }
+        );
+        // console.log(this.covid.data.forecast);
+
+        let date = range(1, 31);
+
+        this.chartdata = {
+          labels: date,
+          datasets: [
+            {
+              label: "Kasus Positif",
+              data: this.covid.data.train,
+              backgroundColor: "transparent",
+              borderColor: "#FF6384"
+            },
+            {
+              label: "Prediksi",
+              data: this.covid.data.forecast,
+              backgroundColor: "transparent",
+              borderColor: "#36A2EB"
+            }
+          ]
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 };
 </script>
