78ff9de808f16512f20c4644898e84e87e2a771a
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-02-08 13:34:04 +0700

    chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>Peramalan</h1>
+  <div class="max-w-xl mx-auto">
+    <div class="p-4 text-center">
+      <h1 class="text-3xl text-indigo-600 font-bold">
+        Peramalan Kasus Positif Covid 19 di Indonesia
+      </h1>
+    </div>
+    <div>
+      <line-chart ></line-chart>
+    </div>
   </div>
 </template>
+
+<script>
+import LineChart from "@/components/line-chart";
+
+export default {
+  components: {
+    'line-chart': LineChart
+  }
+};
+</script>
