2da8f2238e3292604a3e94459696cbb7751150f4
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-02-13 00:22:21 +0700

    save

--- pages/index.vue
+++ pages/index.vue
@@ -77,33 +77,122 @@ export default {
   methods: {
     async getSeries() {
       try {
-        let res = await this.$axios.get(
-          "https://apicovid19indonesia-v2.vercel.app/api/indonesia/harian"
-        );
-        this.series = res.data.slice(-30);
+        // let res = await this.$axios.get(
+        //   "https://apicovid19indonesia-v2.vercel.app/api/indonesia/harian"
+        // );
+        // this.series = res.data.slice(-30);
+        this.series = [
+          { name: "1-Jan", dirawat_kumulatif: 111005 },
+          { name: "2-Jan", dirawat_kumulatif: 110400 },
+          { name: "3-Jan", dirawat_kumulatif: 110679 },
+          { name: "4-Jan", dirawat_kumulatif: 110089 },
+          { name: "5-Jan", dirawat_kumulatif: 110693 },
+          { name: "6-Jan", dirawat_kumulatif: 112593 },
+          { name: "7-Jan", dirawat_kumulatif: 114766 },
+          { name: "8-Jan", dirawat_kumulatif: 117704 },
+          { name: "9-Jan", dirawat_kumulatif: 120928 },
+          { name: "10-Jan", dirawat_kumulatif: 122873 },
+          { name: "11-Jan", dirawat_kumulatif: 123636 },
+          { name: "12-Jan", dirawat_kumulatif: 126313 },
+          { name: "13-Jan", dirawat_kumulatif: 129628 },
+          { name: "14-Jan", dirawat_kumulatif: 133149 },
+          { name: "15-Jan", dirawat_kumulatif: 138238 },
+          { name: "16-Jan", dirawat_kumulatif: 143517 },
+          { name: "17-Jan", dirawat_kumulatif: 145482 },
+          { name: "18-Jan", dirawat_kumulatif: 144798 },
+          { name: "19-Jan", dirawat_kumulatif: 146842 },
+          { name: "20-Jan", dirawat_kumulatif: 149388 },
+          { name: "21-Jan", dirawat_kumulatif: 151658 },
+          { name: "22-Jan", dirawat_kumulatif: 156683 },
+          { name: "23-Jan", dirawat_kumulatif: 158751 },
+          { name: "24-Jan", dirawat_kumulatif: 162617 },
+          { name: "25-Jan", dirawat_kumulatif: 161636 },
+          { name: "26-Jan", dirawat_kumulatif: 163526 },
+          { name: "27-Jan", dirawat_kumulatif: 164113 },
+          { name: "28-Jan", dirawat_kumulatif: 166540 },
+          { name: "29-Jan", dirawat_kumulatif: 170017 },
+          { name: "30-Jan", dirawat_kumulatif: 174083 },
+          { name: "31-Jan", dirawat_kumulatif: 175095 },
+          { name: "1-Feb", dirawat_kumulatif: 175349 },
+          { name: "2-Feb", dirawat_kumulatif: 172576 },
+          { name: "3-Feb", dirawat_kumulatif: 175236 },
+          { name: "4-Feb", dirawat_kumulatif: 174798 },
+          { name: "5-Feb", dirawat_kumulatif: 176672 }
+        ];
       } catch (e) {
         console.log(e);
       }
     },
     async predictCovid() {
-      let values = map(this.series, 'dirawat_kumulatif');
+      let values = map(this.series, "dirawat_kumulatif");
 
       // himpunan semesta
-      let dMin = min(values)
-      let dMax = max(values)
-      let n = values.length
-      let intervalCount = Math.round(1 + 3.322 * Math.log(n))
-      let intervalLength = (dMax - dMin) / intervalCount
+      let dMin = min(values);
+      let dMax = max(values);
+      let n = values.length;
+      let intervalCount = Math.round(1 + 3.322 * Math.log10(n));
+      let intervalLength = (dMax - dMin) / intervalCount;
 
-      console.log('intervalCount', intervalCount)
-      console.log('intervalLength', intervalLength)
+      console.log("dMin", dMin);
+      console.log("dMax", dMax);
+      console.log("intervalCount", intervalCount);
+      console.log("intervalLength", intervalLength);
+
+      // interval
+      let intervals = [];
+      for (let i = 0; i < intervalCount; i++) {
+        const low = dMin + intervalLength * i;
+        const high = dMin + intervalLength * (i + 1);
+        const median = (high - low) / 2 + low;
+        const a = `A${i + 1}`;
+
+        intervals.push({ low, high, median, a });
+      }
+
+      console.log(intervals);
+
+      // FLR
+      this.series.map((seri, iSeries) => {
+        // fuzzifikasi
+        for (let i = 0; i < intervals.length; i++) {
+          const interval = intervals[i];
+
+          if (
+            seri.dirawat_kumulatif >= interval.low &&
+            seri.dirawat_kumulatif <= interval.high
+          ) {
+            seri.fuzzifikasi = interval.a;
+            break;
+          }
+        }
+
+        // relasi
+        if (iSeries !== 0) {
+          const previousSeri = this.series[iSeries - 1];
+
+          seri.relasi = `${previousSeri.fuzzifikasi} > ${seri.fuzzifikasi}`;
+        }
+      });
+
+      console.table(this.series);
+
+      // FRG
+      let group = [];
+
+      for (let i = 0; i < intervals.length; i++) {
+        const interval = intervals[i];
+
+        const name = `Group ${i + 1}`;
+        const relation = 1;
+
+        group.push();
+      }
 
       // let intervalValues = dMin
       // let middleValues = null
       // times(intervalCount, () => {
 
       // })
-
 
       //   try {
       //     this.covid = await this.$axios.post(
